190d355a9594905c1aa797d98857348ef8a7385a
Tri Nguyen <nvutri@gmail.com>
2017-08-22 14:28:08 +0700

    Resolve issue #42. Implement adding Western Union and PayPal as options. Adding total as fees changing.

--- src/freelancer/WithdrawalForm.js
+++ src/freelancer/WithdrawalForm.js
@@ -12,18 +12,38 @@ import { RequestConfig } from '../Config'
 class WithdrawalForm extends Component {
   constructor(props) {
     super(props);
+    this.MIN_AMOUNT = 5.00;
+    this.MAX_AMOUNT = 5000;
     this.state = {
       validationErrors: {},
+      fee: this.MIN_AMOUNT,
+      isValid: true,
+      method: 'wu',
     };
+    this.methodConfig = {
+      'wu': {
+        'label': 'Western Union',
+        'fee': 5.00
+      },
+      'paypal': {
+        'label': 'PayPal',
+        'fee': 3.00
+      }
+    }
     this.withdrawalMethods = [
       {'value': 'wu', 'label': 'Western Union'},
+      {'value': 'paypal', 'label': 'PayPal'},
     ];
   }
   componentDidMount() {
     const requestInstance = request.defaults(RequestConfig);
     const self = this;
     requestInstance.get('/api/withdrawals/balance/').then( (response) => {
-      self.setState(response)
+      const newState = update(response, {$merge: {
+        'amount': response.available,
+        'total_amount': response.available + self.state.fee,
+      }});
+      self.setState(newState)
     });
   }
   submit(data) {
@@ -42,6 +62,32 @@ class WithdrawalForm extends Component {
         msg: err.message
       });
     });
+  }
+
+  handleAmountChange(e) {
+    const newAmount = parseFloat(e.target.value);
+    if (newAmount) {
+      this.setState({
+        amount: newAmount,
+        total_amount: newAmount + this.state.fee,
+        isValid: newAmount >= this.MIN_AMOUNT && newAmount <= this.MAX_AMOUNT
+      })
+    } else {
+      this.setState({
+        amount: e.target.value,
+        total_amount: this.state.fee,
+        isValid: false
+      })
+    }
+  }
+
+  handleSelectChange(name, value) {
+    const newFee = this.methodConfig[value].fee;
+    this.setState({
+      'method': value,
+      'fee': newFee,
+      'total_amount': this.state.amount + newFee
+    })
   }
 
   render() {
@@ -69,7 +115,8 @@ class WithdrawalForm extends Component {
                 name="method"
                 label="Method"
                 options={this.withdrawalMethods}
-                value={this.withdrawalMethods[0].value}
+                value={this.state.method}
+                onChange={this.handleSelectChange.bind(this)}
               />
               <FRC.Input
                 name="amount"
@@ -78,13 +125,26 @@ class WithdrawalForm extends Component {
                 placeholder="Amount"
                 validations="isNumeric"
                 addonBefore="$"
-                value={this.state.available}
+                min={this.MIN_AMOUNT}
+                max={this.MAX_AMOUNT}
+                onKeyUp={this.handleAmountChange.bind(this)}
+                value={this.state.amount}
               />
               <FRC.Input
                 name="fee"
-                label="Fee"
-                placeholder="Withdrawal Fee"
-                value="5.00"
+                label={`${this.methodConfig[this.state.method].label} Fee`}
+                type="number"
+                value={this.state.fee}
+                addonBefore="$"
+                disabled
+              />
+              <FRC.Input
+                name="total_amount"
+                label="Total Amount"
+                type="number"
+                min={this.MIN_AMOUNT}
+                max={this.MAX_AMOUNT}
+                value={this.state.total_amount}
                 addonBefore="$"
                 disabled
               />
@@ -119,13 +179,13 @@ class WithdrawalForm extends Component {
                   value={this.props.user.phone_number}
                 />
               </Well>
-              <Button bsStyle="primary"
+              <Button bsStyle={this.state.saving || !this.state.isValid ? "warning" : "primary"}
                 id="save-button"
                 block={true}
                 formNoValidate={true}
                 type="submit"
-                disabled={ this.state.saving }>
-                {this.state.saving ? 'Submitting' :'Submit Withdrawal Request'}
+                disabled={ this.state.saving || !this.state.isValid }>
+                {this.state.saving ? 'Submitting' : this.state.isValid ? 'Submit Withdrawal Request': 'Invalid Amount. Please Fix'}
               </Button>
             </FRC.Form>
             { this.state.msg ?
