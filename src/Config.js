1ffd09168c6fe587770b6ce59d96ec716cf014e9
Tri Nguyen <nvutri@gmail.com>
2017-08-25 12:19:17 +0700

    Add development configuration and prod

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,10 @@
 const RequestConfig = {
-  baseUrl: 'http://fremancer.com',
+  // baseUrl: 'http://fremancer.com',
+  baseUrl: 'http://localhost:8000',
   json: true
 };
 
+// const StripePublicKey = 'pk_live_CuFfnbBTVsujz1sF3iHVUKUp';
 const StripePublicKey = 'pk_test_oGK6r9tcVMDUJ5jn6dGrWpZJ';
 
 module.exports = {
